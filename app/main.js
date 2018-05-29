898897a898fd6c9a1ef5fad643a9a76a31f7c7e5
Darek <dariusz.grund@morefromit.com>
2018-05-29 14:59:02 +0200

    Move logic into component

--- app/main.js
+++ app/main.js
@@ -1,4 +1,52 @@
-var t = new Vue({
+const submissionComponent = {
+    template:
+        `<div style="display: flex; width: 100%;">
+            <figure class="media-left">
+                <img class="image is-64x64"
+                     v-bind:src="submission.submissionImage">
+            </figure>
+            <div class="media-content">
+                <div class="content">
+                    <p>
+                        <strong>
+                            <a v-bind:href="submission.url" class="has-text-info">
+                                {{ submission.title }}
+                            </a>
+                            <span class="tag is-small">#{{ submission.id }}</span>
+                        </strong>
+                        <br>
+                        {{ submission.description }}
+                        <br>
+                        <small class="is-size-7">
+                            Submitted by:
+                            <img class="image is-24x24"
+                                 v-bind:src="submission.avatar">
+                        </small>
+                    </p>
+                </div>
+            </div>
+            <div class="media-right">
+                <span class="icon is-small" @click="upvote2(submission)">
+                    <i class="fa fa-chevron-up"></i>
+                    <strong class="has-text-info">{{ submission.votes }}</strong>
+                </span>
+            </div>
+        </div>`,
+    props: ['submission', 'submissions'],
+    methods: {
+        upvote(submissionId) {
+            const submission = this.submissions.find(
+                submission => submission.id === submissionId
+            );
+            submission.votes++;
+        },
+        upvote2(submission) {
+            submission.votes++;
+        }
+    }
+};
+
+new Vue({
     el: '#app',
     data: {
         submissions: Seed.submissions
@@ -7,19 +55,17 @@ var t = new Vue({
         sortedSubmissions() {
             return this.submissions.sort((a, b) => {
                 return b.votes - a.votes;
-            })
+            });
         }
     },
     methods: {
-        upvote (submissionId) {
-            const submission = this.submissions.find(
-                submission => submission.id === submissionId
-            );
-            submission.votes++;
-        },
-        upvote2 (submission) {
+        upvote2(submission) {
             submission.votes++;
         }
+    },
+    components: {
+        'submission-component': submissionComponent
     }
 });
+
 
