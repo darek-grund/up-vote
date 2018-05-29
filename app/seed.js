da0aa761a3b1f7c3f8dc54e742d86f4eafce8491
Darek <dariusz.grund@morefromit.com>
2018-05-29 11:55:50 +0200

    initial commit

---
+++ app/seed.js
@@ -0,0 +1,13 @@
+const Seed = {
+    submissions: [
+        {
+            id: 1,
+            title: 'Yellow Pail',
+            description: 'On-demand sand castle construction expertise.',
+            url: '#',
+            votes: 16,
+            avatar: '../public/images/avatars/daniel.jpg',
+            submissionImage: '../public/images/submissions/image-yellow.png',
+        },
+    ]
+};
