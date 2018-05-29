0cd168ee8b1c3bfa6931e6c683ffe6cbe4ae5f90
Darek <dariusz.grund@morefromit.com>
2018-05-29 11:58:53 +0200

    Working app

--- app/main.js
+++ app/main.js
@@ -1,7 +1,25 @@
-new Vue({
+var t = new Vue({
     el: '#app',
     data: {
         submissions: Seed.submissions
+    },
+    computed: {
+        sortedSubmissions() {
+            return this.submissions.sort((a, b) => {
+                return b.votes - a.votes;
+            })
+        }
+    },
+    methods: {
+        upvote (submissionId) {
+            const submission = this.submissions.find(
+                submission => submission.id === submissionId
+            );
+            submission.votes++;
+        },
+        upvote2 (submission) {
+            submission.votes++;
+        }
     }
 });
 
